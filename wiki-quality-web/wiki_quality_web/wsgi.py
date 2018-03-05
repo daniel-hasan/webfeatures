ad2d266a6ab6d1612c26659a47889741e9896c8d
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-03-04 23:42:29 -0300

    Add env variables

--- wiki-quality-web/wiki_quality_web/wsgi.py
+++ wiki-quality-web/wiki_quality_web/wsgi.py
@@ -11,14 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "wiki_quality_web.settings"  # see footnote [2]
-_application = get_wsgi_application()
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "wiki_quality_web.settings" 
 
 def application(environ, start_response):
     # pass the WSGI environment variables on through to os.environ
     for key in environ:
         if key.startswith('wqual_'):
             os.environ[key] = environ[key]
-    os.environ["wqual_SECRET_KEY"] = "sadkoaskdopakd"
-    return _application(environ, start_response)
+            
+    return get_wsgi_application()(environ, start_response)
