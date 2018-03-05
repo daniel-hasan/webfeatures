28fb3f40bc1ee54946d62b986eda3724c07a753d
daniel-hasan <hdalip@gmail.com>
2018-03-05 13:34:41 -0300

    Adicionando no wsgy a nova path

--- wiki-quality-web/wiki_quality_web/wsgi.py
+++ wiki-quality-web/wiki_quality_web/wsgi.py
@@ -14,6 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ["wqual_SECRET_KEY"] = "XXXXXXXXXX"
 os.environ["wqual_db_PASSWORD"] = "XXXXXXXXXX"
 
+#adiciona o projeto wiki-quality como dependente 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR,"wiki-quality"))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wiki_quality_web.settings")
 
 
