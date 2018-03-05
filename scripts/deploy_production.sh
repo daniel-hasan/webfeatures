ddb3815f1de63af1f16183485cadbac4f5f5e9ef
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-03-05 00:05:04 -0300

    Changing wsgi

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -20,9 +20,14 @@ REVOKE CREATE DROP on wikiquality.* to 'wiki_quality'@'127.0.0.1';
 #Instalação do wsgi
 http://pythonclub.com.br/configurando-ambiente-django-com-apache-e-mod-wsgi.html
 
-ln -s ../sites-available/wsgi_test wsgi_test
-#django enviroment variables wqual_SECRET_KEY ewqual_db_PASSWORD
-vim /etc/enviroment
+#copiar o arquivo ".conf" para a producao
+sudo cp deploy/django_wqual.conf /etc/apache2/sites-available/.
+
+
+#copy the wsgi to tthe right location and set the wqual_SECRET_KEY ewqual_db_PASSWORD correctly
+cp git/wiki-quality/wiki-quality-web/wiki_quality_web/wsgi.py wsgi/wqual.py
+
+
 #criando o ambientevirtual
 virtualenv ~/wqual-env -p /usr/bin/python3
 #ativando o ambiente virtual
@@ -37,7 +42,4 @@ colocar em sudo vim /etc/apache2/envvars :
 #alterar o arquivo init para apontar para as configurações de produção
 vim wiki-quality-web/wiki_quality_web/settings/__init__.py 
 
-#copiar o arquivo ".conf" para a producao
-sudo cp deploy/django_wqual.conf /etc/apache2/sites-available/.
 
-"
