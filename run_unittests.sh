79089f7cde2c01e424fdb94e276902e8aff7257a
Priscilla <priscilla.rm.carmo@gmail.com>
2018-03-15 23:37:21 -0300

    correcao de erros ao rodar os testes 4

--- run_unittests.sh
+++ run_unittests.sh
@@ -2,5 +2,5 @@
 python wiki-quality-web/manage.py test scheduler.tests.tests_doc_io
 python wiki-quality-web/manage.py test scheduler.tests.tests_oldestfirst_io
 python wiki-quality-web/manage.py test scheduler.tests.tests_small_job_first_io
-python wiki-quality-web/manage.py test scheduler.tests.scheduler_ruin_io
+python wiki-quality-web/manage.py test scheduler.tests.scheduler_run_io
 python wiki-quality-web/manage.py shell < scheduler/tests/tests_run_usedfeat
