e28c6e9652776589ead25e5718ed6ee285ddccfd
Aline Cristina <ninanerd15@gmail.com>
2018-05-14 09:37:13 -0300

    add svm com instruções

---
+++ libsvm/tools/Folds/geraFolds.py
@@ -0,0 +1,44 @@
+import csv
+
+lista = []
+dicionario = {}
+
+def write_svm_line(arrLinha,classLabel,f):
+	intIdxFeature = 1
+	f.write(classLabel)
+	for col in arrLinha[1:]:
+		f.write(" {idx}:{val}".format(idx=intIdxFeature,val=col))
+		intIdxFeature += 1		
+
+	f.write("\n")
+with open('MV_results_wiki6.csv', 'r') as ficheiro:
+	reader = list(csv.reader(ficheiro, delimiter=','))
+	
+	for linha in reader[1:]:
+		fold=linha[0]
+		if fold not in dicionario:
+			dicionario[fold] = []
+        	
+		dicionario[fold].append(linha)
+
+dicionarioFeat = {}
+with open('data_wiki.csv', 'r') as ficheiro:
+	reader = list(csv.reader(ficheiro, delimiter=','))
+	
+	for linha in reader[1:]:
+		pageId=linha[0]
+		dicionarioFeat[pageId] = linha
+
+for foldId,arrElementos in dicionario.items():
+	#teste
+	with open("teste.fold"+str(foldId)+".txt", 'w+') as arquivo:
+		for linha in arrElementos:
+			write_svm_line(dicionarioFeat[linha[1]],linha[3],arquivo)
+
+	#treino
+	with open("treino.fold"+str(foldId)+".txt", 'w+') as arquivo:
+		for foldIdJ,arrElementosJ in dicionario.items():
+			for linha in arrElementosJ:
+				if foldIdJ != foldId:
+					write_svm_line(dicionarioFeat[linha[1]],linha[3],arquivo)
+
