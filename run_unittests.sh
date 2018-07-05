64c04d13c3bacaa8b4269b3fd7f6c00699f91d6d
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-07-05 15:20:17 -0300

    Correção testes unitarios

--- run_unittests.sh
+++ run_unittests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python wiki-quality-web/manage.py test wqual.scheduler.tests.tests_doc_io --failfast
+#python wiki-quality-web/manage.py test wqual.scheduler.tests.tests_doc_io --failfast
+python wiki-quality-web/manage.py test wqual.tests --failfast
 
