b023e4bf689b074d600fa75436ff5c715a610ec4
Bia Souza <biasouza.inf@gmail.com>
2019-01-25 19:03:15 -0200

    falha de segmentação no comando tagger_light::load_data_notag(teste)

--- sst-light-0.4/teste.py
+++ sst-light-0.4/teste.py
@@ -8,7 +8,7 @@ class SST:
 		for file in data:
 		
 			os.system("./sst train teste ./DATA/SEM_07.BI ./DATA/WNSS_07.TAGSET 0 3 BIO")
-			os.system("./sst tag teste ./MODELS/SEM07_base_12 ./DATA/WNSS_07.TAGSET 0 BIO")
+			os.system("./sst tag ./MODELS/teste teste ./DATA/WNSS_07.TAGSET 0 BIO")
 		
 
 
@@ -17,7 +17,6 @@ class SST:
 
 ./MODELS/WSJc_base_20 ./DATA/WSJ.TAGSET 
 
-os.system("./sst multitag file 0 0 DATA/GAZ/gazlistall_minussemcor ./MODELS/WSJPOSc_base_20 DATA/WSJPOSc.TAGSET ./MODELS/SEM07_base_12 ./DATA/WNSS_07.TAGSET ./MODELS/WSJc_base_20 ./DATA/WSJ.TAGSET")
 			
 '''
 
