e0821eb949867dbd11e54b1a6607f7ebce7da2e2
rubiotorres <rubiotorres15@gmail.com>
2019-03-11 21:42:40 -0300

    Clustering

--- wiki-quality/feature/featureImpl/graph.py
+++ wiki-quality/feature/featureImpl/graph.py
@@ -132,3 +132,21 @@ class Reciprocity(GraphBasedFeature):
             else:
                 dicresult[index] = 0
         return dicresult
+class Clusterizacao(GraphBasedFeature):
+    def compute_feature(self,grafo):
+        dicresult={}        
+        for index in range(0,len(grafo.getvertices())):
+            number = 0
+            degree = len(grafo.get_vertices_saida(index))
+            lista = grafo.get_vertices_saida(index)
+            for dados in lista:
+                for li in grafo.get_vertices_saida(dados):
+                    if(li in lista):
+                        number+=1
+                    else:
+                        pass
+            if(degree != 0):
+                dicresult[index] =number/degree*(degree-1)
+            else:
+                dicresult[index] = 0
+        return dicresult
