4b1856758faf329c63af259629e59b14cdcb6f6a
Larisse Amorim <lari@localhost.localdomain>
2019-06-21 10:31:12 -0300

    Review Features

--- wiki-quality/feature/featureImpl/review_feature_larisse.py
+++ wiki-quality/feature/featureImpl/review_feature_larisse.py
@@ -1,12 +1,41 @@
 
+'''Conta o numero de revisões de um artigo'''
 class ReviewCount(ReviewBasedFeature):
     def __init__(self,name,description,reference,visibility,text_format,feature_time_per_document,curr_date):
         super().__init__(self,name,description,reference,visibility,text_format,feature_time_per_document)
-        self.date_article = float("inf")
+        self.reviews = 0
+
+    def checkReview(self,review):
+        self.reviews +=1
 
     @abstractmethod
-    def compute_feature(self,):
-        date_article = datetime.datetime.fromtimestamp(self.date_article)
-        age = (self.curr_date-date_article).days
+    def compute_feature(self):
+        return self.reviews
 
-        return age
+'''Conta o numero de revisões feita por usuarios anonimos'''
+class AnonymousReviewCount(ConfigurableParam):
+    def __init__(self,att_name,name,description,default_value,param_type,arr_choices=[]):
+        self.name_reviewer = string("inf")
+        self.num_reviews = 0
+
+    def checkReview(self, review):
+        if(sef.name_reviewer == "null"):
+            self.num_reviews +=1
+
+    @abstractmethod
+    def compute_feature(self):
+        return self.num_reviews
+
+''' Conta o numero de revisões feitas por usuarios registrados '''
+class RegisteredReviewCount(ConfigurableParam):
+    def __init__(self,att_name,name,description,default_value,param_type,arr_choices=[]):
+        self.name_reviewer = string("inf")
+        self.num_reviews = 0;
+
+    def checkReview(self, review):
+        if(self.name_reviewer != "null"):
+            self.num_reviews +=1
+
+    @abstractmethod
+    def compute_feature(self):
+        return self.num_reviews
