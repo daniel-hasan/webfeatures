efe87c31434f83b756b3a142cbea146ece81dff9
Daniel Hasan <prof.daniel.hasan@gmail.com>
2020-04-06 10:59:32 -0300

    Correção bug do feature_utils

--- wiki-quality/utils/feature_utils.py
+++ wiki-quality/utils/feature_utils.py
@@ -4,7 +4,7 @@ def get_class_by_name(name):
 	components = name.split('.')
 	module_name = ".".join(components[0:len(components)-1])
 	mod = __import__(module_name)
-	
+
 	for comp in components[1:]:
 		mod = getattr(mod, comp)
 	return mod
