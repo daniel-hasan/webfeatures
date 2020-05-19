55305a9461bd98954083a429b42896f6035bd242
gabriel3105 <gabrielgoncalves310503@gmail.com>
2020-05-19 19:11:15 -0300

    *

--- wiki-quality/cmd_line_interface/teste.py
+++ wiki-quality/cmd_line_interface/teste.py
@@ -1,10 +1,10 @@
 import json
-from feature.features import FeatureDocumentsReader, FeatureDocumentsWriter, FeatureCalculator
-from feature.features
-from utils.uncompress_data import CompressedFile
+#from feature.features import FeatureDocumentsReader, FeatureDocumentsWriter, FeatureCalculator
+#from feature.features import
+#from utils.uncompress_data import CompressedFile
 
 import unittest
-import CaracterInterface #importamos
+from .CaracterInterface import * #importamos
 
 
 
@@ -13,4 +13,30 @@ def fun(x):
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(fun(3), 5)
+        self.assertEqual(fun(4), 5)
+        
+
+class TestCaracterInterface(unittest.TestCase):
+    def testObtemObjetosFeatures(self):
+        
+        novoCaracterInterface = CaracterInterface()
+        
+        arrNomesFeatures = ["Section count","Subsection count","Complete URL link count"]
+        
+        arr_obj_features = novoCaracterInterface.obtemObjetosFeatures(arrNomesFeatures)
+        
+        self.assertEqual(len(arrNomesFeatures),len(arr_obj_features), "Erro! A lista de objetos de features não saiu como o esperado")
+
+        ok = False
+        for feat,nome in zip(arr_obj_features,arrNomesFeatures):
+            ok=True
+            self.assertEqual(feat.name,nome,"Erro! Nomes apresentam divergência")
+           
+        self.assertTrue(ok,"Certifique-se que as listas não estejam vazias!")
+        
+        
+if __name__ == "__main__":
+    unittest.main()
+                
+        
+        
